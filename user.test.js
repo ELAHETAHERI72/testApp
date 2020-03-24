237ae5eca1c94362fa567e51e1c805e6e35ef930
eligirl <46931044+eligirl@users.noreply.github.com>
2020-03-24 11:48:57 +0430

    add navigate code

--- user.test.js
+++ user.test.js
@@ -1,8 +1,33 @@
 const puppeteer = require('puppeteer')
 
+// test('inorrect-logintest',async()=>{
+//     const browser = await puppeteer.launch({
+//         headless :false,
+//         slowMo:80,
+//         args:['--window-size=1920 , 1080']
 
-test('userTest',async()=>{
-    const browser = await puppeteer.launch({
+//     })
+//     const page = await browser.newPage()
+//     await page.goto('http://185.8.173.146:7070/auth/login')
+//     await page.waitForSelector('form');
+//     await page.click('#username')
+//     await page.type('#username','elahe.ta.72@gmail.com')
+//     await page.click('#password')
+//     await page.type('#password','12345678')
+//     await page.click('button[type = submit]')
+    
+//     await page.waitForSelector('.kIyIzC');
+//     const html = await page.$eval('.kIyIzC', el => el.innerHTML);
+
+//     expect(html).toBe('user or password is wrong');
+    
+// },30000)
+
+describe('',()=>{
+    
+  
+it('ّcorrect-logintest',async()=>{
+    let browser = await puppeteer.launch({
         headless :false,
         slowMo:80,
         args:['--window-size=1920 , 1080']
@@ -10,9 +35,19 @@ test('userTest',async()=>{
     })
     const page = await browser.newPage()
     await page.goto('http://185.8.173.146:7070/auth/login')
+    await page.waitForSelector('form');
     await page.click('#username')
-   let username = await page.type('#username','elahe.ta.72@gmail.com')
+    await page.type('#username','farazjalili@gmail.com')
     await page.click('#password')
-    let password =  await page.type('#password','12345678')
+    await page.type('#password','12345678')
     await page.click('button[type = submit]')
+
+    
+    await page.waitFor('button[type = submit]');
+
+    const url = await page.mainFrame().url();
+    expect(url).toContain('ads'); // Uses Jest
+    
+},30000)
+
 })
